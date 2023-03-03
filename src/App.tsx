776e69b61ea27baee78cbba9d6b54a93920d3aea
Nithya Srinivasan <nithya.srinivasan@aquaq.co.uk>
2023-03-03 16:11:04 +0000

    call api endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+  const [response, setResponse] = useState();
+
+  function callApi {
+    const api = 'https://11nfsd5x34.execute-api.us-east-2.amazonaws.com/messages'
+    const res = await axios.get(api, { params: { TableName: 'FIX-messages-test' } });
+    setResponse(res.data);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +20,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload. Welcome!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={callApi}>Call API</button>
+        <p>{response}</p>
       </header>
     </div>
   );
